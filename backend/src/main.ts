909c9ce101683d498721105712b56930d40ccfbd
Geison Melo <geison82@hotmail.com>
2026-06-28 23:07:35 -0300

    feat: ativando documentacao swagger na api

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Enable CORS to allow your frontend to communicate with the backend
+  // Enable CORS
   app.enableCors({
-    origin: 'http://localhost:5173', // specifically allow your Vite frontend
+    origin: '*', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // required if you are passing cookies or authorization headers
+    credentials: true,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('API Projeto N3')
+    .setDescription('A documentação da API do E-commerce')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
