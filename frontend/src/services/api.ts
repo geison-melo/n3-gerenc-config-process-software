82f5f2bd3833b13b005a9bbe53da4a085023af29
Geison Melo <geison82@hotmail.com>
2026-06-28 22:07:08 -0300

    fix: ajustando URL da API para producao na Vercel

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = import.meta.env.PROD ? '/_/backend' : 'http://localhost:3000';
 
 export const api = {
   get: async (endpoint: string) => {
