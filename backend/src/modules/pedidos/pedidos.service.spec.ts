4c014a1539e9d31188ce1989fecec486794505a2
Geison Melo <geison82@hotmail.com>
2026-06-29 12:19:00 -0300

    correção teste falha CI

--- backend/src/modules/pedidos/pedidos.service.spec.ts
+++ backend/src/modules/pedidos/pedidos.service.spec.ts
@@ -37,6 +37,6 @@ describe('PedidosService', () => {
         clienteId: 1,
       })
     }));
-    expect(result.total).toBe(99); // teste falha CI -
+    expect(result.total).toBe(100); // teste falha CI
   });
 });
